2018c95b1d9e9b108bd514af5c5eb882dbb48c36
Brian Ngo <b_ngo13@u.pacific.edu>
2024-03-05 17:07:21 -0800

    Reusing

--- nerge/src/main.rs
+++ nerge/src/main.rs
@@ -2,7 +2,11 @@ mod csv_interface;
 mod layout_branch;
 mod datamod;
 
+use csv_interface::*;
+use layout_branch::*;
+use datamod::*;
+
 fn main() {
-    let csv_reader = csv_interface::CsvConverter::default();
+    let csv_reader = CsvConverter::default();
     csv_reader.process();
 }
