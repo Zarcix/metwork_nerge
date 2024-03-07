1d850c22661740b36c4a5c193faead8cd4a544dd
Brian Ngo <b_ngo13@u.pacific.edu>
2024-03-06 16:57:54 -0800

    Implemented sort and dropping dups

--- nerge/src/datamod.rs
+++ nerge/src/datamod.rs
@@ -1,38 +1,71 @@
 use std::error::Error;
-
 use configparser::ini::Ini;
+use polars::{frame::DataFrame, series::Series};
 pub struct DataMod {
     ip_ranges: Vec<(String, String, String)>, // Vector<(ip start, ip end, campus)>
 }
 
+impl DataMod {
+    pub fn expose_fn(&self, dataframe: DataFrame) {
+        println!("{}", &self.drop_duplicates(self.sort(dataframe)));
+    }
+
+    fn sort(&self, dataframe: DataFrame) -> DataFrame {
+        log::debug!("Sorting data by IP");
+        return dataframe.sort(["column_5"], false, false).expect("Expect sorted dataframe");
+    }
+
+    fn drop_duplicates(&self, dataframe: DataFrame) -> DataFrame {
+        log::debug!("Removing duplicates");
+        return dataframe.unique_stable(Some(&["column_2".into()]), polars::frame::UniqueKeepStrategy::First, None).expect("Expected dataframe without duplicates");
+    }
+
+    fn get_data_indices(&self, dataframe: DataFrame) -> Vec<(String, String, String)> {
+        let data_indices = Vec::new();
+
+        for ip_range in &self.ip_ranges {
+
+        }
+
+        return data_indices;
+    }
+
+    fn parse_data(&self, dataframe: Series, start_ip: String, end_ip: String) {
+        let starting_index = 0;
+        for data in dataframe.iter() {
+            println!("{data}")
+        }
+    }
+}
+
 impl Default for DataMod {
     fn default() -> Self {
-        let mut ip_range_vec = Vec::new();
-
-        parse_config(&mut ip_range_vec).ok();
-
-        log::debug!("Loaded ip range: {ip_range_vec:?}");
+        let ip_range = parse_config().ok().expect("Expected valid ip range from config.txt");
+        log::debug!("Loaded ip range: {ip_range:?}");
 
         Self {
-            ip_ranges: ip_range_vec,
+            ip_ranges: ip_range,
         }
     }
 }
 
 /// Loads a config list from config.txt. If there is no file, a new file will be created
-fn parse_config(ip_vec: &mut Vec<(String, String, String)>) -> Result<(), Box<dyn Error>> {
+fn parse_config() -> Result<Vec<(String, String, String)>, Box<dyn Error>> {
+    let mut ip_vec: Vec<(String, String, String)> = Vec::new();
     let mut config = Ini::new();
     config.load_and_append("config.txt").ok();
 
     { // Add headers if they don't exist
         let expected_config_headers = vec!["STK", "SAC", "SF"];
+        // Check if the cofnig file contains the headers
         if config.sections().iter().all(|section| expected_config_headers.contains(&section.as_str())) {
             log::debug!("Not all headers found. Headers found: {:?}. Adding...", config.sections());
+            // Add in headers and write it back out to save
             for header in expected_config_headers {
                 config.set(header, "Range", Some("[]".into()));
             }
+            config.write("config.txt")?;
         }
-        config.write("config.txt")?;
     }
 
     { // Push all values into the main ip list
@@ -48,7 +81,6 @@ fn parse_config(ip_vec: &mut Vec<(String, String, String)>) -> Result<(), Box<dy
             ip_vec.push((ip_range.0, ip_range.1, "San Francisco".into()))
         }
     }
-    
 
-    Ok(())
+    Ok(ip_vec)
 }
--- nerge/src/main.rs
+++ nerge/src/main.rs
@@ -4,6 +4,7 @@ mod datamod;
 
 use csv_interface::*;
 use layout_branch::*;
+use polars::series::Series;
 use simple_logger::*;
 
 fn main() {
@@ -17,8 +18,14 @@ fn init_logger() {
 
 fn process_data() {
     let csv_reader = CsvConverter::default();
-    let base_dataframe = csv_reader.process();
+    let mut base_dataframe = csv_reader.process();
+    base_dataframe = base_dataframe.drop_many(&["column_4", "column_7", "column_9", "column_10", "column_11"]);
+    let header = base_dataframe.slice(0, 8);
+    let mut body = base_dataframe.slice(7, usize::MAX);
+    body.insert_column(0, Series::new_null("Campus", body.height())).ok();
+    println!("{body}");
     route_layout(&base_dataframe);
 
     let a = datamod::DataMod::default();
+    a.expose_fn(body);
 }
