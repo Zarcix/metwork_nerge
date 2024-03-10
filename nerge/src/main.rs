ff16f6296d28df671e034c7875b321b96d9b3434
Brian Ngo <b_ngo13@u.pacific.edu>
2024-03-10 00:58:22 -0800

    Changed lib

--- nerge/src/main.rs
+++ nerge/src/main.rs
@@ -5,7 +5,6 @@ mod datamod;
 use csv_interface::*;
 use layout_branch::*;
 use polars::series::Series;
-use simple_logger::*;
 
 fn main() {
     init_logger();
@@ -13,17 +12,16 @@ fn main() {
 }
 
 fn init_logger() {
-    SimpleLogger::new().init().unwrap();
+    env_logger::init();
 }
 
 fn process_data() {
     let csv_reader = CsvConverter::default();
     let mut base_dataframe = csv_reader.process();
+    // Old Layout
     base_dataframe = base_dataframe.drop_many(&["column_4", "column_7", "column_9", "column_10", "column_11"]);
     let header = base_dataframe.slice(0, 8);
     let mut body = base_dataframe.slice(7, usize::MAX);
-    body.insert_column(0, Series::new_null("Campus", body.height())).ok();
-    println!("{body}");
     route_layout(&base_dataframe);
 
     let a = datamod::DataMod::default();
